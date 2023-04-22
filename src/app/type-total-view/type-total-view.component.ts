d96ebfa3fafabd0781ec50ab208165480c36fe33
itaasa <itaasarvin@gmail.com>
2023-04-22 15:05:39 -0400

    added progress bar with color differentiation, fixed query date

--- src/app/type-total-view/type-total-view.component.ts
+++ src/app/type-total-view/type-total-view.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { BudgetType, TypeTotalViewModel } from '../budget-entry';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-type-total-view',
   templateUrl: './type-total-view.component.html',
-  styleUrls: ['./type-total-view.component.scss']
+  styleUrls: ['./type-total-view.component.scss'],
 })
 
 //TODO: Create ViewModel for this to determine if the type total font text should be coloured
@@ -17,10 +18,19 @@ export class TypeTotalViewComponent implements OnInit {
     type: BudgetType.Want,
     total: 0,
     maxTotal: 0,
-  }
+  };
 
-  constructor() { }
+  barPercentage = 0;
+  barColor: ThemePalette = 'primary';
+
+  constructor() {}
 
   ngOnInit(): void {
+    if (this.typeTotal.maxTotal) {
+      this.barPercentage =
+        (this.typeTotal.total / this.typeTotal.maxTotal) * 100;
+    }
+
+    this.barColor = this.barPercentage > 100 ? 'warn' : 'primary';
   }
 }

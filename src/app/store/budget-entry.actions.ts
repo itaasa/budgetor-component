db30737993df7c14ac361f58303080efbb045417
itaasa <itaasarvin@gmail.com>
2023-04-22 00:43:20 -0400

    store changes for interval totals

--- src/app/store/budget-entry.actions.ts
+++ src/app/store/budget-entry.actions.ts
@@ -1,60 +1,74 @@
 import { createAction, props } from '@ngrx/store';
-import { BudgetEntry, Interval, TypeTotalViewModel } from '../budget-entry';
+import {
+  BudgetEntry,
+  Interval,
+  IntervalTotal,
+  TypeTotalViewModel,
+} from '../budget-entry';
 
 export const setQueryDate = createAction(
-    `[Budget Entry] Set Query Date`,
-    props<{ queryDate: Date }>(),
+  `[Budget Entry] Set Query Date`,
+  props<{ queryDate: Date }>()
 );
 
 export const setQueryInterval = createAction(
-    `[Budget Entry] Set Interval Date`,
-    props<{ queryInterval: Interval }>(),
+  `[Budget Entry] Set Interval Date`,
+  props<{ queryInterval: Interval }>()
 );
 
 export const loadBudgetEntries = createAction(
-    `[Budget Entry] Load Budget Entries`,
+  `[Budget Entry] Load Budget Entries`
 );
 
 export const loadBudgetEntriesSuccess = createAction(
-    `[Budget Entry] Load Budget Entries Success`,
-    props<{ budgetEntries: BudgetEntry[] }>(),
+  `[Budget Entry] Load Budget Entries Success`,
+  props<{ budgetEntries: BudgetEntry[] }>()
 );
 
 export const addBudgetEntry = createAction(
-    `[Budget Entry] Add Budget Entry`,
-    props<{ budgetEntry: BudgetEntry}>(),
+  `[Budget Entry] Add Budget Entry`,
+  props<{ budgetEntry: BudgetEntry }>()
 );
 
 export const addBudgetEntrySuccess = createAction(
-    `[Budget Entry] Add Budget Entry Success`,
-    props<{ budgetEntry: BudgetEntry }>(),
+  `[Budget Entry] Add Budget Entry Success`,
+  props<{ budgetEntry: BudgetEntry }>()
 );
 
 export const updateBudgetEntry = createAction(
-    `[Budget Entry] Update Budget Entry`,
-    props<{ budgetEntry: BudgetEntry }>(),
+  `[Budget Entry] Update Budget Entry`,
+  props<{ budgetEntry: BudgetEntry }>()
 );
 
 export const updateBudgetEntrySuccess = createAction(
-    `[Budget Entry] Update Budget Entry Success`,
-    props<{ budgetEntry: BudgetEntry }>(),
+  `[Budget Entry] Update Budget Entry Success`,
+  props<{ budgetEntry: BudgetEntry }>()
 );
 
 export const deleteBudgetEntry = createAction(
-    `[Budget Entry] Delete Budget Entry`,
-    props<{ budgetEntry: BudgetEntry }>(),
+  `[Budget Entry] Delete Budget Entry`,
+  props<{ budgetEntry: BudgetEntry }>()
 );
 
 export const deleteBudgetEntrySuccess = createAction(
-    `[Budget Entry] Delete Budget Entry Success`,
-    props<{ budgetEntry: BudgetEntry }>(),
+  `[Budget Entry] Delete Budget Entry Success`,
+  props<{ budgetEntry: BudgetEntry }>()
 );
 
 export const loadTypeTotalViewModels = createAction(
-    `[Budget Entry] Load Type Total View Models`,
+  `[Budget Entry] Load Type Total View Models`
 );
 
 export const loadTypeTotalViewModelsSuccess = createAction(
-    `[Budget Entry] Load Type Total View Models Success`,
-    props<{ typeTotalViewModels: TypeTotalViewModel[] }>(),
+  `[Budget Entry] Load Type Total View Models Success`,
+  props<{ typeTotalViewModels: TypeTotalViewModel[] }>()
+);
+
+export const loadIntervalTotal = createAction(
+  `[Budget Entry] Load Interval Totals`
+);
+
+export const loadIntervalTotalSuccess = createAction(
+  `[Budget Entry] Load Interval Totals Success`,
+  props<{ intervalTotal: IntervalTotal }>()
 );
